47617e2414dfc7b94aa4793dcaa44a6437aaac61
张锋 <zhangfeng930101@qq.com>
2018-08-01 08:52:10 +0800

    sss

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Button } from 'antd'
+import ButtonGroup from '../../node_modules/antd/lib/button/button-group';
 
 class slider extends React.Component {
   render() {
     return (
       <div>
-        <Button type="primary">Primary</Button>
-        <Button type="danger">danger</Button>
+        {/* <Button type="primary" disabled="true">Primary</Button>
+        <Button type="danger" ghost="false">danger</Button>
+        <Button shape="circle" icon="search" loading></Button> */}
+        <ButtonGroup>
+          <Button  ghost>L</Button>
+          <Button  ghost>O</Button>
+          <Button  ghost>V</Button>
+          <Button  ghost>E</Button>
+          <Button  ghost>小</Button>
+          <Button  ghost>r</Button>
+        </ButtonGroup>
+
       </div>
     )
   }
