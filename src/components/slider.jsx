f15c3f5c10abf08144757b7474b85616bcef1f9b
张锋 <zhangfeng930101@qq.com>
2018-08-20 19:02:17 +0800

    sss

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -6,15 +6,6 @@ class slider extends React.Component {
   render() {
     return (
       <div>
-        <div className="grid-box">
-          <span className="item1">111</span>
-          <span className="item2">222</span>
-          <span className="item3">333</span>
-          <span className="item4">444</span>
-          <span className="item5">555</span>
-          <span className="item6">666</span>
-        </div>
-        <div className="shandow shadow-box"></div>
       </div>
     )
   }
