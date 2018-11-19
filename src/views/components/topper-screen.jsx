b35a77a2caa9dcb8c8882e93ebd6c152549adab4
张锋 <zhangfeng930101@qq.com>
2018-11-19 18:49:19 +0800

    sss

--- src/views/components/topper-screen.jsx
+++ src/views/components/topper-screen.jsx
@@ -6,11 +6,17 @@ import Search from 'antd/lib/input/Search';
 const { MonthPicker, RangePicker, WeekPicker } = DatePicker
 // const Search = Input.Search
 class TopperScreen extends React.Component {
+  state = {
+    dateTime: ''
+  }
+  hanldeChangeDate(date, dateString) {
+    console.log(date.format('YYYY-MM-DD HH:mm:ss'), dateString)
+  }
   render() {
     return (
       <div className="topper">
         <div className="time-item1">
-          <DatePicker placeholder="选择年月日"/>
+          <DatePicker placeholder="选择年月日" onChange={this.hanldeChangeDate}/>
         </div>
         <div className="time-item2">
           <MonthPicker placeholder="选择月份"/>
@@ -22,7 +28,7 @@ class TopperScreen extends React.Component {
           <WeekPicker placeholder="选择星期"/>
         </div>
         <div className="search-item">
-          <Input />
+          <Input placeholder="请输入名称"/>
           <Icon type="search" className="search-icon"/>
         </div>
       </div>
