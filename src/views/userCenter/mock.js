4a3fd5736217fb7dfcad05cc263525261aed76a3
张锋 <zhangfeng930101@qq.com>
2018-11-28 16:58:58 +0800

    s

---
+++ src/views/userCenter/mock.js
@@ -0,0 +1,21 @@
+export const personList = [{
+  id: 0,
+  title: '学历',
+  text: '本科'
+}, {
+  id: 1,
+  title: '工作经验',
+  text: '小菜鸟'
+}, {
+  id: 2,
+  title: '兴趣爱好',
+  text: '一个不务正业的吉他手码农'
+}, {
+  id: 3,
+  title: '性格特点',
+  text: '幽默风趣，古怪搞笑，乐观积极'
+}, {
+  id: 4,
+  title: '专业技能',
+  text: '撩妹大法好'
+}]
