a24042fcd106820a2c4af506f0c95acf136352d0
张锋 <zhangfeng930101@qq.com>
2019-01-02 14:15:11 +0800

    redux

---
+++ src/store/actions.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * nujey
+ **/
+
+export function setPageTitle (data) {
+  return (dispatch, getState) => {
+    dispatch({ type: 'SET_PAGE_TITLE', data: data })
+  }
+}
+
+export function setInfoList (data) {
+  return (dispatch, getState) => {
+    window.fetch('/api/getInfoList', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      return res.json()
+    }).then(data => {
+      let { code, data } = data
+      if (code === 200) {
+        dispatch({ type: 'SET_INFO_LIST', data: data })
+      }
+    })
+  }
+}
---
+++ src/store/reducers.js
@@ -0,0 +1,31 @@
+/**
+ * @file reducers
+ */
+
+// 工具函数 用于组织多个reducer， 并返回reducer集合
+import { combineReducers } from 'react-redux'
+
+// 引入默认值
+import defaultState from './state'
+
+// 一个reducer 实际上就是一个函数
+function pageTitle (state = defaultState.pageTitle, action) {
+  // 不同的action 有不同的处理逻辑
+  switch (action.type) {
+    case 'SET_PAGE_TITLE':
+      return action.data
+    default:
+      return state
+  }
+}
+
+function infoList (state = defaultState.infoList, action) {
+  if (action.type) {
+    return action.data
+  } else {
+    return state
+  }
+}
+
+// 导出所有的reducer
+export default combineReducers({ pageTitle, infoList })
