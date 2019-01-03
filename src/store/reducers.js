f97db27e076f5c1bd643c5dd0edb4afc5c035500
张锋 <zhangfeng930101@qq.com>
2019-01-03 09:22:59 +0800

    redux

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,7 +3,7 @@
  */
 
 // 工具函数 用于组织多个reducer， 并返回reducer集合
-import { combineReducers } from 'react-redux'
+import { combineReducers } from 'redux'
 
 // 引入默认值
 import defaultState from './state'
@@ -20,6 +20,7 @@ function pageTitle (state = defaultState.pageTitle, action) {
 }
 
 function infoList (state = defaultState.infoList, action) {
+  console.log(state)
   if (action.type) {
     return action.data
   } else {
