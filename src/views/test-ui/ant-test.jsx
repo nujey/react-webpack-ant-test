be91a36e66900cfac77bc7f7e9c9db95eb4fa52f
张锋 <zhangfeng930101@qq.com>
2019-02-22 17:46:48 +0800

    grid-template-areas ;

--- src/views/test-ui/ant-test.jsx
+++ src/views/test-ui/ant-test.jsx
@@ -91,6 +91,11 @@ class TestUi extends Component {
           <div className="grid-item">2</div>
           <div className="grid-item">3</div>
           <div className="grid-item">4</div>
+
+          <div className="grid-item">5</div>
+          <div className="grid-item">6</div>
+          <div className="grid-item">7</div>
+          <div className="grid-item">8</div>
         </div>
       </div>
     )
